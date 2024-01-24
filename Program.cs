6c4a90f2c6f6cfb22e1b009ad973cacee1468856
loukeeehh <louka.constant@gmail.com>
2024-01-24 15:33:30 +0100

    Mes_Tris_De_Tableaux

---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using System.Diagnostics;
+
+namespace _5ti_LoukaConstant_triTableaux
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans cette methode de tris format tableau !");
+            int[] T;
+            int tailleTableau;
+            string nUser;
+            string repeatProg;
+            string message;
+            Methodedetri mestris = new Methodedetri();
+
+            do
+            {
+                Console.WriteLine(" Pour commencer, quel méthode de tri voulez - vous ?");
+                Console.WriteLine(" - Taper 1 pour le tri pas sélection.\n - Taper 2 pour le tri par insertion.\n - Taper 3 pour le tri intuitif.\n - Taper 4 pour le tri shell.\n - Taper 5 pour le tri à bulles.\n");
+                nUser = Console.ReadLine();
+
+                if (nUser == "1")
+                {
+                    Console.WriteLine("Vous avez choisi le tri par sélection !");
+                    Console.WriteLine("Combien de cases voulez-vous ?");
+                    tailleTableau = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Voici le tableau de base :");
+                    mestris.tableauAleatoire(tailleTableau, out T);
+                    mestris.afficherTableau(T, out message);
+                    Console.WriteLine(message);
+
+                    //traitement
+                    mestris.triparSelection(ref T);
+
+                    Console.WriteLine("Voici le tableau après le tri :");
+                    mestris.afficherTableau(T, out message);
+                    
+
+                    Stopwatch chrono = new Stopwatch();
+                    long millisec;
+                    chrono = Stopwatch.StartNew();
+                    mestris.triparSelection(ref T);
+                    chrono.Stop();
+                    millisec = chrono.ElapsedMilliseconds;
+                    Console.WriteLine(message);
+                    Console.WriteLine($"{millisec} millisecondes");
+
+                }
+
+                else if (nUser == "2")
+                {
+                    Console.WriteLine("Combien de cases voulez-vous ?");
+                    tailleTableau = int.Parse(Console.ReadLine());
+
+
+                    Console.WriteLine("Voici le tableau de base :");
+                    mestris.tableauAleatoire(tailleTableau, out T);
+                    mestris.afficherTableau(T, out message);
+                    Console.WriteLine(message);
+
+                    //traitement
+                    mestris.triparInsertion(T);
+
+                    Console.WriteLine("Voici le tableau après le tri :");
+                    mestris.afficherTableau(T, out message);
+                    
+
+                    Stopwatch chrono = new Stopwatch();
+                    long millisec;
+                    chrono = Stopwatch.StartNew();
+                    mestris.triparInsertion(T);
+                    chrono.Stop();
+                    millisec = chrono.ElapsedMilliseconds;
+                    Console.WriteLine(message);
+                    Console.WriteLine($"{millisec} millisecondes");
+
+                }
+
+
+
+                else if (nUser == "3")
+                {
+                    Console.WriteLine("Vous avez choisi le tri intuitif !");
+                    Console.WriteLine("Combien de cases voulez-vous ?");
+                    tailleTableau = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Voici le tableau de base :");
+                    mestris.tableauAleatoire(tailleTableau, out T);
+                    mestris.afficherTableau(T, out message);
+                    Console.WriteLine(message);
+
+                    //traitement
+                    mestris.triIntuitif(T);
+
+                    Console.WriteLine("Voici le tableau après le tri :");
+                    mestris.afficherTableau(T, out message);
+                    
+
+                    Stopwatch chrono = new Stopwatch();
+                    long millisec;
+                    chrono = Stopwatch.StartNew();
+                    mestris.triIntuitif(T);
+                    chrono.Stop();
+                    millisec = chrono.ElapsedMilliseconds;
+                    Console.WriteLine(message);
+                    Console.WriteLine($"{millisec} millisecondes");
+
+                }
+
+                else if (nUser == "4")
+                {
+                    Console.WriteLine("Vous avez choisi le tri shell !");
+                    Console.WriteLine("Combien de cases voulez-vous ?");
+                    tailleTableau = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Voici le tableau de base :");
+                    mestris.tableauAleatoire(tailleTableau, out T);
+                    mestris.afficherTableau(T, out message);
+                    Console.WriteLine(message);
+
+                    //traitement
+                    mestris.triShell(T);
+
+                    Console.WriteLine("Voici le tableau après le tri :");
+                    mestris.afficherTableau(T, out message);
+              
+                    Stopwatch chrono = new Stopwatch();
+                    long millisec;
+                    chrono = Stopwatch.StartNew();
+                    mestris.triShell(T);
+                    chrono.Stop();
+                    millisec = chrono.ElapsedMilliseconds;
+                    Console.WriteLine(message);
+                    Console.WriteLine($"{millisec} millisecondes");
+
+                }
+
+                else if (nUser == "5")
+                {
+                    Console.WriteLine("Vous avez choisi le tri à bulles !");
+                    Console.WriteLine("Combien de cases voulez-vous ?");
+                    tailleTableau = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Voici le tableau de base :");
+                    mestris.tableauAleatoire(tailleTableau, out T);
+                    mestris.afficherTableau(T, out message);
+                    Console.WriteLine(message);
+
+                    //traitement
+                    mestris.triABulles(T);
+
+                    Console.WriteLine("Voici le tableau après le tri :");
+                    mestris.afficherTableau(T, out message);
+                   
+                    Stopwatch chrono = new Stopwatch();
+                    long millisec;
+                    chrono = Stopwatch.StartNew();
+                    mestris.triABulles(T);
+                    chrono.Stop();
+                    millisec = chrono.ElapsedMilliseconds;
+                    Console.WriteLine(message);
+                    Console.WriteLine($"{millisec} millisecondes");
+
+                }
+
+                //demande de reprise
+                Console.WriteLine("Voulez-vous recommencer ? 'o' = oui, 'n' = non");
+                repeatProg = Console.ReadLine();
+
+            } while (repeatProg == "o");
+        }
+    }
+}
---
+++ mestris.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5ti_LoukaConstant_triTableaux
+{
+    public struct Methodedetri
+    {
+        public void tableauAleatoire(int tailleTableau, out int[] T)
+        {
+            T = new int[tailleTableau];
+            Random alea = new Random();
+
+
+            for (int i = 0; i < tailleTableau; i++)
+            {
+                T[i] = alea.Next(1, 101);
+            }
+
+
+        }
+
+        public void afficherTableau(int[] T, out string message)
+        {
+            message = "";
+            foreach (int i in T)
+            {
+                message = message + i + " ";
+            }
+        }
+
+
+        public void triparSelection(ref int[] T)
+        {
+            int i;
+            int j;
+            int n; //nbre éléments dans le tableau.
+            int p; //place de l'élément à changer.
+            int echange;
+
+
+            for (i = 0; i <= T.Length - 2; i++)
+            {
+                p = i;
+
+                for (j = i + 1; j <= T.Length - 1; j++)
+                {
+                    if (T[j] < T[p])
+                    {
+                        p = j;
+                    }
+
+
+                }
+
+                if (p != i)
+                {
+                    echange = T[p];
+                    T[p] = T[i];
+                    T[i] = echange;
+
+                }
+
+            }
+        }
+
+        public void triparInsertion(int[] T)
+        {
+            int n;
+            int x;
+            bool bpp;
+            int j;
+            int i;
+
+            for (j = 1; j < T.Length - 1; j++)
+            {
+                x = T[j];
+                i = j - 1;
+                bpp = false;
+
+                while (x < T[i])
+                {
+                    T[i + 1] = T[i];
+
+                    if (i == 0)
+                    {
+                        bpp = true;
+
+                    }
+
+                    else
+                    {
+                        i = i - 1;
+                    }
+
+                    if (bpp)
+                    {
+                        T[0] = x;
+                    }
+
+                    else
+                    {
+                        T[i + 1] = x;
+                    }
+                }
+
+
+            }
+        }
+
+        public void triIntuitif(int[] T)
+        {
+            int echange;
+
+            for (int i = 0; i <= T.Length - 2; i++)
+            {
+                for (int j = i + 1; j <= T.Length - 1; j++)
+                {
+                    if (T[i] > T[j])
+                    {
+
+
+                        echange = T[i];
+                        T[i] = T[j];
+                        T[j] = echange;
+                    }
+                }
+            }
+
+
+
+        }
+
+        public void triShell(int[] T)
+        {
+            int n = T.Length;
+            bool permut;
+            int Ec = n / 2;
+            int x;
+
+            while (Ec >= 1)
+            {
+                do
+                {
+                    permut = false;
+                    for (int i = 0; i <= n - 1 - Ec; i++)
+                    {
+                        if (T[i] > T[i + Ec])
+                        {
+                            x = T[i];
+                            T[i] = T[i + Ec];
+                            T[i + Ec] = x;
+                            permut = true;
+                        }
+                    }
+                } while (permut == true);
+
+                Ec = Ec / 2;
+
+            }
+        }
+
+        public void triABulles(int[] T1)
+        {
+            bool b1 = true;
+            int az;
+
+            while (b1 == true)
+            {
+                b1 = false;
+
+                for (int i = 0; i <= T1.Length - 2; i++)
+                {
+                    if (T1[i] > T1[i + 1])
+                    {
+                        az = T1[i];
+                        T1[i] = T1[i + 1];
+                        T1[i + 1] = az;
+                        b1 = true;
+                    }
+                }
+            }
+        }
+    }
+}
